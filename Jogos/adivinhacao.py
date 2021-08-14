1b6e635be753d97f5509a922f82f2e78afe78f09
Luan Castro Alves <luancastro_alves@hotmail.com>
2021-08-14 13:00:03 -0300

    feat: python

--- Jogos/adivinhacao.py
+++ Jogos/adivinhacao.py
@@ -3,21 +3,29 @@ print("Welcome to Game of adivinhacao!")
 print("******************************")
 
 num_secret = 42
+total_tentativas = 3
 
-chute = int(input("Digite o seu numero: "))
+for rodada in range(1, total_tentativas + 1):
+    print("Tentativa: {} de {}".format(rodada, total_tentativas))
+    chute = int(input("Digite o seu numero: "))
+    print("Voce digitou ", chute)
 
-print("Voce digitou ", chute)
+    if(chute < 1 or chute > 100):
+        print("Voce deve digitar um numero entre 1 e 100!")
+        continue
 
-acertou = chute == num_secret
-isMaior = chute > num_secret
-isMenor = chute < num_secret
-if(acertou):
-    print("Voce acertou!")
-else:
-    if(isMaior):
-        print("Voce errou! O seu chute foi maior do que o numero secreto")
-    elif(isMenor):
-        print("Voce errou! O seu chute foi menor do que o numero secreto")
+    acertou = chute == num_secret
+    isMaior = chute > num_secret
+    isMenor = chute < num_secret
+
+    if(acertou):
+        print("Voce acertou!")
+        break
+    else:
+        if(isMaior):
+            print("Voce errou! O seu chute foi maior do que o numero secreto")
+        elif(isMenor):
+            print("Voce errou! O seu chute foi menor do que o numero secreto")
 
 
 print("Fim Jogo")
---
+++ Jogos/variascoisas.py
@@ -0,0 +1,45 @@
+# print("Ola", "QUE", sep="/", end="\n+")
+
+nome = 'Luan'
+print(f'Meu nome é {nome.lower()}')
+print(f'Meu nome é {nome.upper()}')
+
+
+print("R$ {:6.1f}".format(1000.12))
+print("R$ {:07.2f}".format(4.11))
+
+print("R$ {:.2f}".format(1.59))
+print("R$ {:.2f}".format(50.59))
+print("R$ {:7.2f}".format(1243.59))
+print("R$ {:07.2f}".format(25.59))
+print("R$ {:7d}".format(25))
+print("Data {:02d}/{:02d}".format(5,11))
+print("Data {:02d}/{:02d}/{:02d}".format(19,11,1998))
+
+# for rodada in range(1, 10):
+#     print(rodada)
+
+
+# for rodada in range(1, 10, 2):
+#     print(rodada)
+
+# dia_ini = 24
+# dia_fim = 28
+# mes = "fevereiro"
+# ano = 2017
+
+
+# print("Em {} o Carnaval acontece em {} do dia {} até o dia {}".format(
+#     ano, mes, dia_ini, dia_fim))
+
+
+# usuario = input("Informe o usuário do sistema!")
+
+# if(usuario == "Flávio"):
+#     print("Seja bem-vindo Flávio!")
+# elif(usuario == "Douglas"):
+#     print("Seja bem-vindo Douglas!")
+# elif(usuario == "Nico"):
+#     print("Seja bem-vindo Nico")
+# else:
+#     print("Usuário não identificado!")
