e42554feb9202e7efd3183550a626dbc6c8783ed
Luan Castro Alves <luancastro_alves@hotmail.com>
2021-08-21 20:56:14 -0300

    feat: python part 2

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -1,12 +1,155 @@
+from os import error
 import random
 
 
-def jogar():
+def welcomeForca():
     print("******************************")
     print("Welcome to Game of Forca!")
     print("******************************")
 
-    print("Fim Jogo")
+
+def loading_palavra():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+    arquivo.close()
+    numero = random.randrange(0, len(palavras))
+    return palavras[numero].upper()
+
+
+def init_letras_acertadas(palavra_secret):
+    return ["_" for letra in palavra_secret]
+
+
+def pede_chute():
+    chute = input("Digite uma letra ").upper().strip()
+    return chute
+
+
+def marca_chute_correto(chute, letras_acertadas, palavra_secret):
+    index = 0
+    for letra in palavra_secret:
+        if(chute == letra):
+            letras_acertadas[index] = letra
+        index += 1
+
+
+def imprime_mensagem_vencedor():
+    print("Parabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
+
+def imprime_mensagem_perdedor(palavra_secreta):
+    print("Puxa, você foi enforcado!")
+    print("A palavra era {}".format(palavra_secreta))
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+
+
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
+
+def jogar():
+
+    welcomeForca()
+    palavra_secret = loading_palavra()
+    letras_acertadas = init_letras_acertadas(palavra_secret)
+
+    enforcou = False
+    acertou = False
+    errou = 0
+
+    print(letras_acertadas)
+    while(not enforcou and not acertou):
+
+        chute = pede_chute()
+
+        if(chute in palavra_secret):
+            marca_chute_correto(chute, letras_acertadas, palavra_secret)
+        else:
+            errou += 1
+            desenha_forca(errou)
+        enforcou = errou == 7
+        acertou = "_" not in letras_acertadas
+
+        print(letras_acertadas)
+    if(acertou):
+        imprime_mensagem_vencedor()
+    else:
+        imprime_mensagem_perdedor(palavra_secret)
 
 
 if(__name__ == "__main__"):
--- Jogos/variascoisas.py
+++ Jogos/variascoisas.py
@@ -1,54 +1,84 @@
-print("Ola", "QUE", sep="/", end="\n+")
+# print("Ola", "QUE", sep="/", end="\n+")
 
 
-num1 = 21
-num2 = 32
+arquivo = open("nome_do_arquivo.txt", "m")
+arquivo.close()
+arquivo = open("nome_do_arquivo.txt", "a")
+arquivo.readline()
 
-num3 = abs(num1 - num2) / 3
+for linha in arquivo:
+    print(linha)
+arquivo.close()
+palavra = "teste"
+# palavra2 = palavra.capitalize().strip().lower()
+# palavra.endswith("te")  # true
+# palavra.strip()
+listaT = [1, 5, 8, 9, 6, "teste"]
+listaT.append(8)
+len(listaT)
+dias = ("S", "T", "Q", "Q", "S", "S", "D",)  # tuple
 
-print(round(num3))
-print(3//2)
+p1 = (3, 5)
+p2 = (110, 8)
+p3 = (58, 9)
 
-nome = 'Luan'
-print(f'Meu nome é {nome.lower()}')
-print(f'Meu nome é {nome.upper()}')
+line = [p1, p2, p3]
+line[0][0][0]
 
+lista = ["_" for letra in palavra]
 
-print("R$ {:6.1f}".format(1000.12))
-print("R$ {:07.2f}".format(4.11))
+# precos = [1525, 1120, 1464, 1200, 1330, 1356, 1312, 1531, 1232, 1234, 1250, 1114, 1553, 1147, 1303, 1296, 1309, 1404, 1479, 1376, 1152, 1440, 1038, 1018,
+#           1291, 1388, 1577, 1115, 1488, 1494, 1254, 1230, 1122, 1396, 1208, 1356, 1549, 1116, 1443, 1075, 1536, 1542, 1036, 1015, 1020, 1217, 1484, 1032, 1390, 1026]
+# print(min(precos))
+# print(max(precos))
+# num1 = 21
+# num2 = 32
 
-print("R$ {:.2f}".format(1.59))
-print("R$ {:.2f}".format(50.59))
-print("R$ {:7.2f}".format(1243.59))
-print("R$ {:07.2f}".format(25.59))
-print("R$ {:7d}".format(25))
-print("Data {:02d}/{:02d}".format(5,11))
-print("Data {:02d}/{:02d}/{:02d}".format(19,11,1998))
+# num3 = abs(num1 - num2) / 3
 
-for rodada in range(1, 10):
-    print(rodada)
+# print(round(num3))
+# print(3//2)
 
-
-for rodada in range(1, 10, 2):
-    print(rodada)
-
-dia_ini = 24
-dia_fim = 28
-mes = "fevereiro"
-ano = 2017
+# nome = 'Luan'
+# print(f'Meu nome é {nome.lower()}')
+# print(f'Meu nome é {nome.upper()}')
 
 
-print("Em {} o Carnaval acontece em {} do dia {} até o dia {}".format(
-    ano, mes, dia_ini, dia_fim))
+# print("R$ {:6.1f}".format(1000.12))
+# print("R$ {:07.2f}".format(4.11))
+
+# print("R$ {:.2f}".format(1.59))
+# print("R$ {:.2f}".format(50.59))
+# print("R$ {:7.2f}".format(1243.59))
+# print("R$ {:07.2f}".format(25.59))
+# print("R$ {:7d}".format(25))
+# print("Data {:02d}/{:02d}".format(5,11))
+# print("Data {:02d}/{:02d}/{:02d}".format(19,11,1998))
+
+# for rodada in range(1, 10):
+#     print(rodada)
 
 
-usuario = input("Informe o usuário do sistema!")
+# for rodada in range(1, 10, 2):
+#     print(rodada)
 
-if(usuario == "Flávio"):
-    print("Seja bem-vindo Flávio!")
-elif(usuario == "Douglas"):
-    print("Seja bem-vindo Douglas!")
-elif(usuario == "Nico"):
-    print("Seja bem-vindo Nico")
-else:
-    print("Usuário não identificado!")
+# dia_ini = 24
+# dia_fim = 28
+# mes = "fevereiro"
+# ano = 2017
+
+
+# print("Em {} o Carnaval acontece em {} do dia {} até o dia {}".format(
+#     ano, mes, dia_ini, dia_fim))
+
+
+# usuario = input("Informe o usuário do sistema!")
+
+# if(usuario == "Flávio"):
+#     print("Seja bem-vindo Flávio!")
+# elif(usuario == "Douglas"):
+#     print("Seja bem-vindo Douglas!")
+# elif(usuario == "Nico"):
+#     print("Seja bem-vindo Nico")
+# else:
+#     print("Usuário não identificado!")
