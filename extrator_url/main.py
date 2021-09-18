6d8d6a3f9fccf0d6bc8872e984cc8c2153d1960b
Luan Castro Alves <luancastro_alves@hotmail.com>
2021-09-18 17:18:46 -0300

    python string

---
+++ extrator_url/main.py
@@ -0,0 +1,9 @@
+url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
+
+
+print(url)
+url_query = url[20:38]
+print(url_query)
+
+url_base = url[0:20]
+print(url_base)
