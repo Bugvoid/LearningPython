42e3936d5017cf5720e3062849af90081bcb9003
Luan Castro Alves <luancastro_alves@hotmail.com>
2021-09-19 16:43:01 -0300

    feat: python string

--- extrator_url/main.py
+++ extrator_url/main.py
@@ -1,9 +1,41 @@
-url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
+url = "https://bytebank.com/cambio?quantidade=100&moedaDestino=dolar&moedaOrigem=real"
 
 
-print(url)
-url_query = url[20:38]
-print(url_query)
+# print(url)
+# url_query = url[20:38]
+# print(url_query)
 
-url_base = url[0:20]
-print(url_base)
+# url_base = url[0:20]
+# print(url_base)
+
+
+# indices = url.find("?")
+# url_params = url[:indices]
+# url_params2 = url[indices+1:]
+
+# print(url_params)
+# print(url_params2)
+
+# Separa base e os parametros
+indice_interrogacao = url.find("?")
+url_base = url[:indice_interrogacao]
+url_parametros = url[indice_interrogacao+1:]
+print(url_parametros)
+
+# Busca o valor de parametro
+parametro_busca = "quantidade"
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find("&", indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+
+print(valor)
+
+url = "https://www.alura.com.br/curso?curso=python"
+indice_curso = url.find("curso")
+indice_valor = indice_curso + len("curso") + 1
+valor = url[indice_valor:]
+print(valor)
